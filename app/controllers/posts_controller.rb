207b4746b6793e74f24e941b76eb9aa8f0915514
Chuks Opia <chuks24ng@yahoo.co.uk>
2018-05-20 14:07:35 +0100

    - add post controller code - add post model code

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,26 @@
 class PostsController < ApplicationController
   def index
+    @posts = Post.all.order(created_at: :desc)
   end
 
   def new
+    @post = Post.new
   end
 
   def create
+    @post = Post.new(post_params)
+
+    respond_to do |format|
+      if @post.save
+        format.json { render :show, status: :created }
+      else
+        format.json { render json: @chat.errors, status: :unprocessable_entity }
+      end
+    end
   end
+
+  private
+    def post_params
+      params.require(:post).permit(:post, :username)
+    end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,8 @@
 class Post < ApplicationRecord
+  after_create :notify_pusher, on: :create
   has_many :likes
+
+  def notify_pusher
+    Pusher.trigger('feed', 'new-post', self.as_json)
+  end
 end
